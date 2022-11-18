ef3516f2ed0a371e9dbb34d05ee9ea887eccec99
sophharris1998 <117087805+sophharris1998@users.noreply.github.com>
2022-11-18 22:55:29 +0000

    equals and clear buttons functioning

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const screenDisplay = document.querySelector("#display");
 let storeNumber = 0;
-
 let initalClick = true;
+let lastStoredOperation = "";
 
 //This makes all numbers display when clicked
 const buttonsArray = document.querySelectorAll("#calcButton");
@@ -19,7 +19,14 @@ const calcButtonClear = document.querySelector("#calcButtonClear");
 const clearDisplay = () => {
   screenDisplay.innerText = "";
 };
-calcButtonClear.addEventListener("click", clearDisplay);
+
+//This is going to reset the storednumber back to 0
+const clearDisplayStoredNumber = () => {
+  storeNumber = 0;
+  screenDisplay.innerText = "";
+};
+
+calcButtonClear.addEventListener("click", clearDisplayStoredNumber);
 
 //This makes the plus symbol work on the Calculator
 const calcButtonPlus = document.querySelector("#calcButtonPlus");
@@ -32,14 +39,70 @@ const plusNumber = () => {
   }
   clearDisplay();
   console.log(storeNumber);
+  lastStoredOperation = "+=";
 };
 calcButtonPlus.addEventListener("click", plusNumber);
 
 //This makes the plus symbol work on the Calculator
 const calcButtonMinus = document.querySelector("#calcButtonMinus");
 const minusNumber = () => {
-  storeNumber -= parseInt(screenDisplay.innerText);
+  if (initalClick == true) {
+    storeNumber = parseInt(screenDisplay.innerText);
+    initalClick = false;
+  } else {
+    storeNumber -= parseInt(screenDisplay.innerText);
+  }
   clearDisplay();
   console.log(storeNumber);
+  lastStoredOperation = "-=";
 };
 calcButtonMinus.addEventListener("click", minusNumber);
+
+//This makes the multiply symbol work on the Calculator
+const calcButtonTimes = document.querySelector("#calcButtonTimes");
+const timesNumber = () => {
+  if (initalClick == true) {
+    storeNumber = parseInt(screenDisplay.innerText);
+    initalClick = false;
+  } else {
+    storeNumber *= parseInt(screenDisplay.innerText);
+  }
+  clearDisplay();
+  console.log(storeNumber);
+  lastStoredOperation = "*=";
+};
+calcButtonTimes.addEventListener("click", timesNumber);
+
+//This makes the divide symbol work on the Calculator
+const calcButtonDivide = document.querySelector("#calcButtonDivide");
+const divideNumber = () => {
+  if (initalClick == true) {
+    storeNumber = parseInt(screenDisplay.innerText);
+    initalClick = false;
+  } else {
+    storeNumber /= parseInt(screenDisplay.innerText);
+  }
+  clearDisplay();
+  console.log(storeNumber);
+  lastStoredOperation = "/=";
+};
+calcButtonDivide.addEventListener("click", divideNumber);
+
+//This makes the equals button show the result of the equation
+const calcButtonEquals = document.querySelector("#calcButtonEquals");
+const theAnswer = () => {
+  if (lastStoredOperation == "+=") {
+    plusNumber();
+  }
+  if (lastStoredOperation == "-=") {
+    minusNumber();
+  }
+  if (lastStoredOperation == "*=") {
+    timesNumber();
+  }
+  if (lastStoredOperation == "/=") {
+    divideNumber();
+  }
+  screenDisplay.innerText = storeNumber;
+};
+calcButtonEquals.addEventListener("click", theAnswer);
